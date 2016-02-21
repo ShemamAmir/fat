e4dd79ab571aa5b7b7ecabe68a8d8c787e24ee38
ShemamAmir <shamamamer@yahoo.com>
2016-02-21 14:14:27 +0300

    Update and rename oflb.py to oflb1.py

--- oflb1.py
+++ oflb1.py
@@ -26,7 +26,7 @@ allList = [ ]
 ##########################################
 #Data center Fat Tree Network Topology
 class dcFatTreeTopo(Topo):
-"Linear topology of k switches, with one host per switch."
+print "Linear topology of k switches, with one host per switch."
 def __init__(self, k=2, **opts):
 """Init.
 k: number of switches (and hosts)
@@ -40,13 +40,13 @@ link3 = dict(bw=1, delay='15ms', loss=0, max_queue_size=100, use_htb=True)
 # Creating an array of core switches
 # Adding them in an array so that they can be refered to later
 for i in irange(0, k-1):
-"core switch"
+print "core switch"
 coreSwitch = self.addSwitch('c%s%s' % (i+1, 0))
 #coreList.insert(i, coreSwitch)
 coreList.append(coreSwitch)
 #lastSwitch = None
 for i in irange(1, k):
-"aggregation switches"
+print "aggregation switches"
 aggSwitch1 = self.addSwitch('a%s%s' % (i, 1))
 aggSwitch2 = self.addSwitch('a%s%s' % (i, 3))
 aggList.append(aggSwitch1)
@@ -61,20 +61,20 @@ host12 = self.addHost('h%s%s' % (i, 2))
 host13 = self.addHost('h%s%s' % (i, 3))
 host14 = self.addHost('h%s%s' % (i, 4))
 #hosts1 = [ net.addHost( 'h%d' % n ) for n in 3, 4 ]
-"connection of the hosts to the left tor switch "
+print "connection of the hosts to the left tor switch "
 self.addLink(host11, torSwitch1, **link3)
 self.addLink(host12, torSwitch1, **link3)
-"connection of the hosts to the right tor switch "
+print "connection of the hosts to the right tor switch "
 self.addLink(host13, torSwitch2, **link3)
 self.addLink(host14, torSwitch2, **link3)
-"connection of the the left tor switch to aggregation switches"
+print "connection of the the left tor switch to aggregation switches"
 self.addLink(torSwitch1, aggSwitch1, **link2)
-27
+
 self.addLink(torSwitch1, aggSwitch2, **link2)
-"connection of the the right tor switch to aggregation switches"
+print "connection of the the right tor switch to aggregation switches"
 self.addLink(torSwitch2, aggSwitch1, **link2)
 self.addLink(torSwitch2, aggSwitch2, **link2)
-"connect the aggregation switch to top pod core switch"
+print "connect the aggregation switch to top pod core switch"
 if k == 1:
 for r in irange(0, k): #this is to go through the agg switches
 self.addLink(aggList[r], coreList[0])
@@ -94,13 +94,13 @@ for sw in allList:
 print allList[sw]
 def simpleTest():
 # argument to put in either remote or local controller
-"Create and test a simple network"
+print "Create and test a simple network"
 c0 = RemoteController( 'c0', ip='192.168.90.146' )
 # the cmap here needs to dynamically take the switch name from the switchLists[] so that it is not static
 #cmap = { 'a11': c0, 'a12': c0, 'a21': c0, 'a22': c0, 'a31': c0, 'a32': c0, 'a41': c0, 'a42': c0, 'c11': c0, 'c21': c0, 'c31': c0,
-'c41': c0, 't11': c0, 't12': c0, 't21': c0, 't22': c0, 't31': c0, 't32': c0, 't41': c0, 't42': c0}
+#'c41': c0, 't11': c0, 't12': c0, 't21': c0, 't22': c0, 't31': c0, 't32': c0, 't41': c0, 't42': c0}
 class MultiSwitch( OVSSwitch ):
-"Custom Switch() subclass that connects to different controllers"
+print "Custom Switch() subclass that connects to different controllers"
 def start( self, controllers ):
 return OVSSwitch.start( self, [ cmap[ self.name ] ] )
 #section for handling the differnt argumetns.... simpleTest(arg1, arg2, ...) will take in arguments from user
